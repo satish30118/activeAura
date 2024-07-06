7ea65bcf7acb2a3472978eb6031f13f0f452760c
Satish Maurya <satishmaurya30118@gmail.com>
2024-07-06 20:40:55 +0530

    user controlller fixes

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,8 @@
-const getProfile = require("../controller/UserController");
+const {getProfile, addFriend} = require("../controller/UserController");
 const Authorization = require("../middleware/authorization");
 const router = require("express").Router();
 
 router.route("/get-friends").get(Authorization, getProfile);
-router.route("/add-friend").get(Authorization, getProfile.addFriend);
+router.route("/add-friend").get(Authorization, addFriend);
 
 module.exports = router;
